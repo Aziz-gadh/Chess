677549e91e6a52027f2b5ed5f91ca228399b07b9
Mohamed Aziz Gadhgadhi <mohamedaziz.ghadgadhi@insat.ucar.tn>
2026-04-21 23:41:59 +0100

    adjustment

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 import pygame as pg
 
-side=75
+side=80
 board_side=side*8
 FPS=60
 board=pg.sprite.Group()
 black_square_color='#769656'
 white_square_color='#EEEED2'
-margin=(0,75)
+margin=(40,80)
